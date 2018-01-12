600b8d6e3e15baa84f255902768c55b2d8d17810
leol <ducleoliao@gmail.com>
2018-01-11 17:35:03 -0800

    09 palindrome

--- leetcode/09_palindrome_number.rb
+++ leetcode/09_palindrome_number.rb
@@ -1,5 +1,15 @@
+# Determine whether an integer is a palindrome. Do this without extra space.
+
 def is_palindrome(x)
-    x - x.to_s.reverse.to_i == 0
+    # x - x.to_s.reverse.to_i == 0
+
+    return false if (x < 0 || (x % 10 == 0 && x !=0))
+    revertedNumber = 0 
+    while x > revertedNumber
+        revertedNumber = revertedNumber * 10 + x % 10;
+        x/=10;
+    end 
+    return x == revertedNumber || x == revertedNumber/10
 end
 
 p is_palindrome(10101) == true 
