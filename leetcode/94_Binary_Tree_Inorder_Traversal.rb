eb02479dcaaebb6440465061d27fd1ed6b19030f
leol <ducleoliao@gmail.com>
2018-01-13 01:18:51 -0800

    Improved 94

--- leetcode/94_Binary_Tree_Inorder_Traversal.rb
+++ leetcode/94_Binary_Tree_Inorder_Traversal.rb
@@ -12,8 +12,8 @@
 #     end
 # end
 
+## Recursive solution
 def inorder_traversal(root)
-  ## Recursive solution
     #Base: when root is at the end
     return [] if root.nil?
     #Declare empty array
@@ -30,3 +30,47 @@ end
 
 #Time Complexity: O(n). The acutal recursvie function is T(n) = 2 * T(n/2) + 1
 #Space Complexity: Worst case space O(n). Average O(log(n))
+
+## Iterating method using Stack // Needs improvement
+def inorder_traversal_i(root)
+    # declare a queue (Stack)'
+    # declare an array variable
+    # until root.value.nil? 
+      # push all the left nodes to the stack
+    # once reach to root.value, push stack value to array  
+    arr = []
+    stack = Queue.new 
+    currentNode = root
+    while (!currentNode.nil? || !stack.empty?)
+      while !currentNode.nil?
+        stack.push(currentNode)
+        currentNode = root.left
+      end 
+      currentNode = stack.pop 
+      arr.push(currentNode.val)
+      currentNode = currentNode.right 
+    end 
+    return arr
+
+end
+
+#Time Complexity: O(n). The acutal recursvie function is T(n) = 2 * T(n/2) + 1
+#Space Complexity: Worst case space O(n). Average O(log(n))
+
+# Test Case 
+class TreeNode
+  attr_reader :val
+  attr_accessor :left, :right
+
+  def initialize(val)
+    @val = val
+    @left, @right = nil, nil
+  end
+end
+
+root = TreeNode.new(1)
+root.right = TreeNode.new(2)
+root.right.left = TreeNode.new(3)
+
+
+p inorder_traversal(root)
