d24d1096d5146ee998e5c5969e1f25c095a424b8
leol <ducleoliao@gmail.com>
2018-01-12 22:09:06 -0800

    Added comments

--- leetcode/94_Binary_Tree_Inorder_Traversal.rb
+++ leetcode/94_Binary_Tree_Inorder_Traversal.rb
@@ -1,5 +1,5 @@
 # Given a binary tree, return the inorder traversal of its nodes' values.
-# 
+# https://leetcode.com/problems/binary-tree-inorder-traversal/description/
 # For example:
 # Given binary tree [1,null,2,3],
 # return [1,3,2]
@@ -13,11 +13,20 @@
 # end
 
 def inorder_traversal(root)
+  ## Recursive solution
+    #Base: when root is at the end
     return [] if root.nil?
+    #Declare empty array
     result = []
+    #push recursive call of root.left
     result.concat(inorder_traversal(root.left))
+    #push root vale itself
     result << root.val
+    #push recursive call of root.right
     result.concat(inorder_traversal(root.right))
+    # return result 
     result
 end
 
+#Time Complexity: O(n). The acutal recursvie function is T(n) = 2 * T(n/2) + 1
+#Space Complexity: Worst case space O(n). Average O(log(n))
