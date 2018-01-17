9c4994c79e017e0e747309c35fb25a47f6525c6b
leol <ducleoliao@gmail.com>
2018-01-17 00:10:33 -0800

    Added notes to median of two sorted array

--- UnderstandJSTrickyParts/javascript.md
+++ UnderstandJSTrickyParts/javascript.md
@@ -11,3 +11,10 @@
 ## What is difference between .proto vs __proto__
 - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/proto
 
+## Understand prototype inheritance: 
+Resource: `https://github.com/tianyuduan/JS30/blob/master/PrototypalInheritance/prototype.md` 
+- JS Object inherit their properties and methods from their prototype.
+- The prototype property is an object, and it has a constructor property by default which allows yo to add new properties and methods to existing objects types.
+- __proto__ : when an object is created in JAvaScript, JavaScript Engine adds a __proto__ property to the newly created object; it points to the prototype object of the constructor function. 
+
+
--- leetcode/04_median_of_two_sorted_arrays.rb
+++ leetcode/04_median_of_two_sorted_arrays.rb
@@ -14,25 +14,51 @@
 
 def find_median_sorted_arrays(nums1, nums2)
   ## imagine that your data is merged [1,2,3,4,5,6,7,8,9]
-  ## Thenm after the first 4 numbers, the next one is the median. 
+  ## Then after the first 4 numbers, the next one is the median. 
   ##      Remaining    Remaining     Last used    Last used
   # i    a[i, ...]   b[4-i, ...]    from b       from a
-  # 0    [2,3,6,8]           [9]    7            None
-  # 1      [3,6,8]         [7,9]    5            2
+  # 0    [2,3,6,8]           [9]    7            None         // Violates Rule 1
+  # 1      [3,6,8]         [7,9]    5            2            // Violates Rule 1
   # 2        [6,8]       [5,7,9]    4            3
   # 3          [8]     [4,5,7,9]    1            6
   # 4           []   [1,4,5,7,9]    None         8
 
-  
+  # To make the above possible, there are two rules:
+  # 1. The last numbe rused from b can't be larger than the next remaining number in a (because it would've been a mistake ot use it first).
+  # 2. The last number used from a can't be larger than the next remaining number in b.
+
+  # After merging the first four numbers, we must be in some status.
+
+  # m1 + m2 = k = (n1 + n2 + 1) / 2 # length of the array
+  # Median must be from {A(m1-1), A(m1), B(m2-1), B(m2)}
+  # Binary Search m1, base on a[m1] < ?b[m2-1]
+  # C(k-1) = [A(m1-1), B(m2-1)].max
+  # C(k)   = [A(m1), B(m2)].min
+
+  # Sort the arrays by its size
   a, b = [nums1, nums2].sort_by(&:size)
+  # figure out the length of each array input
   m, n = a.size, b.size
-  after = (m + n - 1) / 2
-  i = (0...m).bsearch { |i| after-i-1 < 0 || a[i] >= b[after-i-1] } || m
-  nextfew = (a[i,2] + b[after-i,2]).sort
+  # figure the median number of combination of two array
+  k = (m + n - 1) / 2
+  # Use binary search to find one that obeys rule 1
+  # next remaing number in a is a[i]
+  # last used number from b is b (k-i-1)
+  i = (0...m).bsearch { |i| k-i-1 < 0 || a[i] >= b[k-i-1] } || m
+  # take the next(up to) two number from each, and sort that
+  nextfew = (a[i,2] + b[k-i,2]).sort
+  # take the first number and test and whether the array is odd
   (nextfew[0] + nextfew[1 - (m+n)%2]) / 2.0
+
 end
 
 p find_median_sorted_arrays([1,3], [2]) == 2.0
 p find_median_sorted_arrays([1,2], [3, 4])  == 2.5
 p find_median_sorted_arrays([], [1])  == 1.0
-p find_median_sorted_arrays([2,3,6,8], [1,4,5,7,9])
+p find_median_sorted_arrays([1,2,3], [4,5,6])  == 3.5 ## 3,4
+p find_median_sorted_arrays([1,2], [3,4,5])  == 3.0 ## 3
+p find_median_sorted_arrays([1,2], [3,4,5, 6])  == 3.5 ## 3,4
+p find_median_sorted_arrays([1,2], [3,4,5,6,7])  == 4.0 ## 4
+p find_median_sorted_arrays([2,4], [1,3,5])  == 3.0 ## 3
+p find_median_sorted_arrays([9,10], [2,4,6])  == 6.0 ## 6
+p find_median_sorted_arrays([2,3,6,8], [1,4,5,7,9]) == 5.0
