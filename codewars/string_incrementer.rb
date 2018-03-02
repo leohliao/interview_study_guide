6ca2422637a3bdd0159fe0375356caaff0ddb483
leol <ducleoliao@gmail.com>
2018-03-01 16:59:57 -0800

    Solved string_incrementer

--- codewars/string_incrementer.rb
+++ codewars/string_incrementer.rb
@@ -16,11 +16,13 @@
 
 def increment_string(input)
   # declare a str and int variable
-  return "1" if input.nil?
+  return "1" if input.empty?
 
   numbers = Hash.new(0)
   10.times {|n| numbers[n.to_s]=true }
   idx = nil
+  str, int = ""
+  
   # iterate the input
   (0...input.length).each do |i|
     # find the index of the end of string
@@ -30,17 +32,41 @@ def increment_string(input)
     end 
   end
   # separate the string and the integer
-  str = input[0..idx- 1]
+  return input + "1" if idx.nil?
+
+  str = input[0..idx-1]
   int = input[idx..-1]
-  p str
-  p int 
+  
   # increment the integer
+  parsed_int = parseInt(int)
+
   # combine the string and integer again
+  return str + parsed_int
+  
 end
 
-# p increment_string("foo")#  == "foo1"
-p increment_string("foobar001")# == "foobar002"
-# p increment_string("foobar1")# == "foobar2"
-# p increment_string("foobar00")# == "foobar01"
-# p increment_string("foobar99")# == "foobar100"
-# p increment_string("")# == "1"
+def parseInt(str)
+  parsed_int = ""
+  zero_counts = 0 
+  (0...str.length).each do |i|
+    if str[i] === "0"
+      zero_counts += 1
+    end
+  end 
+
+  inc_str = (str.to_i + 1).to_s
+  if inc_str.length > str.delete("0").length
+    zero_counts -= (inc_str.length - str.delete("0").length)
+  end
+  
+  zero_counts.times {|i| parsed_int << "0"}
+  parsed_int << inc_str
+  parsed_int
+end
+
+p increment_string("foo") == "foo1"
+p increment_string("foobar001") == "foobar002"
+p increment_string("foobar1") == "foobar2"
+p increment_string("foobar00") == "foobar01"
+p increment_string("foobar99") == "foobar100"
+p increment_string("") == "1"
