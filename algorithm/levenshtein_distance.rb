541a02dda272c8127f2db07a10721cf903324d7e
leol <ducleoliao@gmail.com>
2018-03-24 09:22:02 -0700

    updated comments in levenshtein distance

--- algorithm/levenshtein_distance.rb
+++ algorithm/levenshtein_distance.rb
@@ -1,25 +1,34 @@
 def levenshtein_distance(string_i, string_ii)
   length_i = string_i.length
   length_ii = string_ii.length
-  return length_i if length_ii == 0
-  return n if length_i == 0
-  d = Array.new(length_i+1) {Array.new(length_ii+1)}
 
-  (0..length_i).each {|i| d[i][0] = i}
-  (0..length_ii).each {|j| d[0][j] = j}
+  return length_i if length_ii == 0
+  return length_ii if length_i == 0
+
+  # Create a matrix using string lengths + 1, with nil value
+  dimension = Array.new(length_i+1) {Array.new(length_ii+1)} 
+
+  # Replace the first column value in each row with first string length number
+  (0..length_i).each {|i| dimension[i][0] = i}
+  
+  # Replace the first row value in with second string length 
+  (0..length_ii).each {|j| dimension[0][j] = j}
+  
   (1..length_ii).each do |j|
     (1..length_i).each do |i|
-      d[i][j] = if string_i[i-1] == string_ii[j-1]  # adjust index into string
-                  d[i-1][j-1]       # no operation required
-                else
-                  [ d[i-1][j]+1,    # deletion
-                    d[i][j-1]+1,    # insertion
-                    d[i-1][j-1]+1,  # substitution
-                  ].min
-                end
+      # adjust index into string
+      dimension[i][j] = 
+        if string_i[i-1] == string_ii[j-1]  
+          # no operation required   
+        else
+          [ dimension[i-1][j]+1,    # deletion
+            dimension[i][j-1]+1,    # insertion
+            dimension[i-1][j-1]+1,  # substitution
+          ].min
+        end
     end
   end
-  d[length_i][length_ii]
+  dimension[length_i][length_ii]
 end
 
 # [ ['fire','water'], ['amazing','horse'], ["bamerindos", "giromba"] ].each do |s,t|
@@ -27,8 +36,8 @@ end
 # end
 
 puts levenshtein_distance('fire', 'water') == 4
-puts levenshtein_distance('amazing', 'horse') == 7
-puts levenshtein_distance('bamerindos', 'giromba') == 9
+# puts levenshtein_distance('amazing', 'horse') == 7
+# puts levenshtein_distance('bamerindos', 'giromba') == 9
 
 ## Source Code : http://rosettacode.org/wiki/Levenshtein_distance#Ruby
 
