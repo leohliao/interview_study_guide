41a1d636e8f6963b0efa3a0be7adc832c5fb2c17
Leo <ducleoliao@gmail.com>
2022-09-17 10:18:50 -0700

    Leetcode 001 Two Sum

--- leetcode/001_two_sum.js
+++ leetcode/001_two_sum.js
@@ -20,8 +20,6 @@ Input: nums = [-1,-2,-3,-4,-5], target = -8
 Output: [2,4]
 
 
-
-
 --- Constraint:
 
     2 <= nums.length <= 104
@@ -31,3 +29,20 @@ Output: [2,4]
 Q: Will me always have positive integers in the array?
 
 */ 
+
+// Approach 
+// Time: O(N)
+// Space: O(N)
+var twoSum = function(nums, target) {
+    const target_diff = {};
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        const diff = target - num;
+
+        if (target_diff[num.toString()] >= 0) {
+            return [target_diff[num.toString()], i];
+        } else {
+            target_diff[diff] = i;
+        }
+    }
+};
